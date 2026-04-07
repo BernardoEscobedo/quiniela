26dcce8f7bb38441e3c595c26cf7ba95732d632e
BernardoEscobedo <berna.lsc20@gmail.com>
2026-04-07 17:10:51 -0600

    middleware 1

--- middlewares/jwt.middlewares.js
+++ middlewares/jwt.middlewares.js
@@ -1,1 +1,25 @@
-ssl
+import jwt from "jsonwebtoken"
+
+export const verifyToken = (req, res, next) => {
+    let token = req.headers.authorization
+
+    if(!token || !token.startsWith('Bearer')){
+        return res.status(401).json({error: "Formato de token invalido"})
+    }
+
+    console.log({token})
+    token = token.split(" ")[1]
+
+    console.log({token})
+
+    try {
+        const {correo, id_role} = jwt.verify(token, process.env.JWT_SECRET)
+        console.log(correo)
+        req.correo = correo
+        req.id_role = id_role
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({error: "Token invalido"})
+    }
+} 
